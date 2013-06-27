5141856f280f246a7876b155ce8d8dfac0ddb5bd
roy <roy@laptop-roy.(none)>
2013-06-27 07:23:20 +0200

    Add organisation edit.

---
+++ vanenapp/src/main/java/com/roybraam/vanenapp/stripes/OrganisationActionBean.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.roybraam.vanenapp.stripes;
+
+import com.roybraam.vanenapp.entity.Organisation;
+import java.util.List;
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.action.Before;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.SimpleMessage;
+import net.sourceforge.stripes.action.StrictBinding;
+import net.sourceforge.stripes.action.UrlBinding;
+import net.sourceforge.stripes.controller.LifecycleStage;
+import net.sourceforge.stripes.validation.Validate;
+import net.sourceforge.stripes.validation.ValidateNestedProperties;
+import org.stripesstuff.stripersist.Stripersist;
+
+/**
+ *
+ * @author Roy Braam
+ */
+@StrictBinding
+@UrlBinding("/action/admin/organisation/{$event}")
+public class OrganisationActionBean implements ActionBean {
+    
+    private static final String JSP = "/WEB-INF/jsp/admin/organisation.jsp";
+    private ActionBeanContext context;
+    private List<Organisation> organisations;
+    
+    @Validate(on = {"save"})   
+    @ValidateNestedProperties({
+        @Validate(on = {"save"}, field = "name", required = true, maxlength = 255, label = "Naam")
+    })
+    private Organisation organisation;
+    
+    @Before(stages = LifecycleStage.BindingAndValidation)
+    public void load() {
+        organisations = Stripersist.getEntityManager().createQuery("from Organisation").getResultList();
+    }
+    
+    @DefaultHandler
+    public Resolution view(){
+        return new ForwardResolution(JSP);
+    }
+    
+    public Resolution edit(){
+        return new ForwardResolution(JSP);
+    }
+
+    public Resolution save() {
+        Stripersist.getEntityManager().persist(getOrganisation());
+        Stripersist.getEntityManager().getTransaction().commit();
+        this.load();
+        getContext().getMessages().add(new SimpleMessage("Organisatie is opgeslagen"));
+        return new ForwardResolution(JSP);
+    }
+    public Resolution cancel() {
+        return new ForwardResolution(JSP);
+    }
+
+    public Resolution delete() {
+        Stripersist.getEntityManager().remove(this.getOrganisation());
+        Stripersist.getEntityManager().getTransaction().commit();
+        this.load();
+        getContext().getMessages().add(new SimpleMessage("Organisatie is verwijderd"));
+        return new ForwardResolution(JSP);
+    }
+
+    
+    //<editor-fold defaultstate="collapsed" desc="Getters and Setters">
+    @Override
+    public ActionBeanContext getContext() {
+        return context;
+    }
+    
+    @Override
+    public void setContext(ActionBeanContext context) {
+        this.context = context;
+    }
+    public List<Organisation> getOrganisations() {
+        return organisations;
+    }
+
+    public void setOrganisations(List<Organisation> organisations) {
+        this.organisations = organisations;
+    }
+    
+    public Organisation getOrganisation() {
+        return organisation;
+    }
+
+    public void setOrganisation(Organisation organisation) {
+        this.organisation = organisation;
+    }
+    //</editor-fold>
+
+
+}
---
+++ vanenapp/src/main/webapp/WEB-INF/jsp/admin/organisation.jsp
@@ -0,0 +1,64 @@
+<%-- 
+    Author     : Roy Braam
+--%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@include file="/WEB-INF/jsp/taglibs.jsp"%>
+
+<stripes:layout-render name="/WEB-INF/jsp/templates/main.jsp">
+    <stripes:layout-component name="head">
+        <title>Organisatie beheer</title>
+    </stripes:layout-component>
+
+    <stripes:layout-component name="header">
+        <jsp:include page="/WEB-INF/jsp/header.jsp"/>
+    </stripes:layout-component>
+    <stripes:layout-component name="menu">
+        <jsp:include page="/WEB-INF/jsp/admin/menu.jsp"/>
+    </stripes:layout-component>
+
+    <stripes:layout-component name="body">
+        <p>
+            <stripes:errors/>
+            <stripes:messages/>
+        </p>
+        <h1>Beheer Organisaties</h1><br/>
+        <div class="admin-list">
+            <table>
+                <tr>
+                <th>Naam</th>
+                <th></th>
+                </tr>
+                <c:forEach items="${actionBean.organisations}" var="o">
+                    <tr>
+                        <td><a href='<stripes:url beanclass="com.roybraam.vanenapp.stripes.OrganisationActionBean" event="edit"/>?user=${o.id}'><c:out value="${o.name}"/></a></td>
+                        <td><a href='<stripes:url beanclass="com.roybraam.vanenapp.stripes.OrganisationActionBean" event="delete"/>?user=${o.id}'>Verwijder</a></td>
+                    </tr>
+                </c:forEach>
+            </table>
+        </div>
+        <stripes:form beanclass="com.roybraam.vanenapp.stripes.OrganisationActionBean">
+            <c:choose>
+                <c:when test="${actionBean.context.eventName == 'edit'}">
+                    <stripes:hidden name="organisation" value="${actionBean.organisation.id}"/>
+                    <div class="form-container">
+                        <table class="formtable">                    
+                            <tr>
+                                <td>Naam</td>
+                                <td><stripes:text name="organisation.name"/></td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="submitbuttons">
+                        <stripes:submit name="save" value="Opslaan"/>
+                        <stripes:submit name="cancel" value="Annuleren"/>
+                    </div>
+                </c:when>
+                <c:otherwise>
+                    <stripes:submit name="edit" value="Nieuwe organisatie"/>
+                </c:otherwise>
+            </c:choose>
+        </stripes:form>
+
+    </stripes:layout-component>
+
+</stripes:layout-render>
