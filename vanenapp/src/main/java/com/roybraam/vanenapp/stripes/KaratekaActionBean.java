a12193c8743bc15d4a85c84d16c5744ade4a5d64
Roy Braam <braam.roy@gmail.com>
2013-07-03 17:41:42 +0200

    First step in creating karateka mutation features

---
+++ vanenapp/src/main/java/com/roybraam/vanenapp/stripes/KaratekaActionBean.java
@@ -0,0 +1,86 @@
+package com.roybraam.vanenapp.stripes;
+
+import com.roybraam.vanenapp.entity.Karateka;
+import com.roybraam.vanenapp.entity.Kyu;
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.SimpleMessage;
+import net.sourceforge.stripes.action.StrictBinding;
+import net.sourceforge.stripes.action.UrlBinding;
+import net.sourceforge.stripes.validation.Validate;
+import net.sourceforge.stripes.validation.ValidateNestedProperties;
+import org.stripesstuff.stripersist.Stripersist;
+
+/**
+ *
+ * @author Roy Braam
+ */
+@StrictBinding
+@UrlBinding("/action/admin/karateka/{$event}")
+public class KaratekaActionBean implements ActionBean{
+    private String JSP = "/WEB-INF/jsp/admin/karateka.jsp";
+    private String EDITJSP = "/WEB-INF/jsp/admin/karatekaedit.jsp";
+    private ActionBeanContext context;
+    
+    @Validate(on = {"save"})
+    @ValidateNestedProperties({
+        @Validate(on = {"save"}, field = "name", required = true, maxlength = 255, label = "Naam"),
+        @Validate(on = {"save"}, field = "surname", required = true, maxlength = 255, label = "Achternaam"),
+        @Validate(on = {"save"}, field = "insert", required = true, maxlength = 255, label = "Tussenvoegsel"),
+        @Validate(on = {"save"}, field = "gender", required = true, maxlength = 255, label = "Geslacht"),
+        @Validate(on = {"save"}, field = "birthdate", required = true, maxlength = 255, label = "Geboortedatum"),
+        @Validate(on = {"save"}, field = "weight", label = "Gewicht")
+    })
+    private Karateka karateka;
+    @Validate(on ={"save"}, required = true, label= "Kyu-graad")
+    private String belt;
+
+    @DefaultHandler
+    public Resolution view(){
+        return new ForwardResolution(JSP);
+    }
+    
+    public Resolution edit(){
+        if (karateka!=null){
+            this.belt = karateka.getBelt().name();
+        }
+        return new ForwardResolution(EDITJSP);
+    }
+    
+    public Resolution save(){
+        karateka.setBelt(Kyu.valueOf(this.belt));
+        Stripersist.getEntityManager().persist(karateka);
+        Stripersist.getEntityManager().getTransaction().commit();
+        getContext().getMessages().add(new SimpleMessage("Karateka is opgeslagen"));
+        return new ForwardResolution(EDITJSP);
+    }
+    
+    //<editor-fold defaultstate="collapsed" desc="Getters and Setters">
+    public ActionBeanContext getContext() {
+        return context;
+    }
+    
+    public void setContext(ActionBeanContext context) {
+        this.context = context;
+    }
+    
+    public Karateka getKarateka() {
+        return karateka;
+    }
+    
+    public void setKarateka(Karateka karateka) {
+        this.karateka = karateka;
+    }
+    
+    public String getBelt() {
+        return belt;
+    }
+    
+    public void setBelt(String belt) {
+        this.belt = belt;
+    }
+    //</editor-fold>
+}
